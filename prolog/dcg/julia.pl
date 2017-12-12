2141a083e3675381c0d98a6d05b36a97abbc7c5c
samer-- <samer.abdallah.00@gmail.com>
2017-12-12 01:12:56 +0000

    expand tabs

--- prolog/dcg/julia.pl
+++ prolog/dcg/julia.pl
@@ -2,45 +2,41 @@
  * Part of pljulia
  * Copyright Samer Abdallah 2017
  *
- *	This program is free software; you can redistribute it and/or
- *	modify it under the terms of the GNU General Public License
- *	as published by the Free Software Foundation; either version 2
- *	of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
- *	This program is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *	You should have received a copy of the GNU General Public
- *	License along with this library; if not, write to the Free Software
- *	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * You should have received a copy of the GNU General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 :- module(dcg_julia,
-	[	term_jlstring/2   % (+Expr, -String)  ~Prolog term to Matlab string
-	,	term_texatom/2		% (+Expr, -Atom)         ~Prolog term to TeX expression
+      [  term_jlstring/2   % (+Expr, -String)  ~Prolog term to Matlab string
+      ,  term_texatom/2    % (+Expr, -Atom)         ~Prolog term to TeX expression
 
-	,	op(150,fx,:)		% symbols
-	% ,	op(150,fy,#)		% tuples
-	,	op(160,yf,'`')		% postfix ctranspose operator
-   ,  op(800,xfy,:>:)
-
-	% note slightly reduced precedence of array operators -
-	% hope this doesn't break everything...
-	,	op(210,xfy,.^)	   % array exponentiation
-	,	op(410,yfx,.*)	   % array times
-	,	op(410,yfx,./)    % array division
-	,	op(410,xfy,.\)	   % reverse array division
-	,	op(400,xfy,\)		% reverse matrix division
-	,	op(100,yfx,'`')   % dot, as in modules and fields
-	,  op(750,xfy,\\)    % lambda abdstraction
-	,	op(200,yf,[])		% array indexing
-   ,  op(160,fx,.)	 % broadcasting op
-   ,  op(200,yfx,@)	 % function application
-   ,  op(200,yfx,.@)	 % broadcasting function application
-   ,  op(300,yf,...)  % splicing
-	]).
+      ,  op(150,fx,:)      % symbols
+      ,  op(160,yf,'`')    % postfix ctranspose operator
+      ,  op(800,xfy,:>:)
+      ,  op(210,xfy,.^)    % broadcasting exponentiation
+      ,  op(410,yfx,.*)    % broadcasting times
+      ,  op(410,yfx,./)    % broadcasting division
+      ,  op(410,xfy,.\)    % reverse broadcasting division
+      ,  op(400,xfy,\)     % reverse matrix division
+      ,  op(100,yfx,'`')   % dot, as in modules and fields
+      ,  op(750,xfy,\\)    % lambda abdstraction
+      ,  op(200,yf,[])     % array indexing
+      ,  op(160,fx,.)      % broadcasting op
+      ,  op(200,yfx,@)     % function application
+      ,  op(200,yfx,.@)    % broadcasting function application
+      ,  op(300,yf,...)    % splicing
+      ]).
 
 
 :- multifile user:pl2jl_hook/2, pl2jl_hook/3.
@@ -56,85 +52,85 @@
    REVIEW
     \\ for lambda
 
-	---++++ Types
+   ---++++ Types
 
-	==
+   ==
    X :>: Y :: expr :-  X::expr, Y::expr.
-	X,Y     :: expr :-  X::expr, Y::expr.
-	X=Y     :: expr :-  X::ml_lval, Y::expr.
-	==
+   X,Y     :: expr :-  X::expr, Y::expr.
+   X=Y     :: expr :-  X::ml_lval, Y::expr.
+   ==
 
-	==
-	expr(A)       % A Matlab expression, possibly with multiple return values
-	==
+   ==
+   expr(A)       % A Matlab expression, possibly with multiple return values
+   ==
 
-	---++++ Matlab expression syntax
+   ---++++ Matlab expression syntax
 
-	The Matlab expression syntax adopted by this module allows Prolog terms to represent
-	or denote Matlab expressions. Let T be the domain of recognised Prolog terms (corresponding to
-	the type expr), and M be the domain of Matlab expressions written in Matlab syntax.
-	Then V : T->M is the valuation function which maps Prolog term X to Matlab expression V[X].
-	These are some of the constructs it recognises:
+   The Matlab expression syntax adopted by this module allows Prolog terms to represent
+   or denote Matlab expressions. Let T be the domain of recognised Prolog terms (corresponding to
+   the type expr), and M be the domain of Matlab expressions written in Matlab syntax.
+   Then V : T->M is the valuation function which maps Prolog term X to Matlab expression V[X].
+   These are some of the constructs it recognises:
 
-	==
+   ==
    X:>:Y           % |--> V[X]; V[Y]  (sequential evaluation hiding first result)
-	X=Y             % |--> V[X]=V[Y] (assignment, X must denote a valid left-value)
-	if(X,Y)         % |--> if V[X], V[Y], end
-	if(X,Y,Z)       % |--> if V[X], V[Y], else V[Z], end
+   X=Y             % |--> V[X]=V[Y] (assignment, X must denote a valid left-value)
+   if(X,Y)         % |--> if V[X], V[Y], end
+   if(X,Y,Z)       % |--> if V[X], V[Y], else V[Z], end
 
-	+X              % |--> uplus(V[X])
-	-X              % |--> uminus(V[X])
-	X+Y             % |--> plus(V[X],V[Y])
-	X-Y             % |--> minus(V[X],V[Y])
-	X^Y             % |--> mpower(V[X],V[Y])
-	X*Y             % |--> mtimes(V[X],V[Y])
-	X/Y             % |--> mrdivide(V[X],V[Y])
-	X\Y             % |--> mldivide(V[X],V[Y])
-	X.^Y            % |--> power(V[X],V[Y])
-	X.*Y            % |--> times(V[X],V[Y])
-	X./Y            % |--> rdivide(V[X],V[Y])
-	X.\Y            % |--> ldivide(V[X],V[Y])
-	X:Y:Z           % |--> colon(V[X],V[Y],V[Z])
-	X:Z             % |--> colon(V[X],V[Z])
-	X>Z             % |--> gt(V[X],V[Y])
-	X>=Z            % |--> ge(V[X],V[Y])
-	X<Z             % |--> lt(V[X],V[Y])
-	X=<Z            % |--> le(V[X],V[Y])
-	X==Z            % |--> eq(V[X],V[Y])
-	[X1,X2,...]     % |--> [ V[X1], V[X2], ... ]
+   +X              % |--> +(V[X])
+   -X              % |--> -(V[X])
+   X+Y             % |--> +(V[X],V[Y])
+   X-Y             % |--> -(V[X],V[Y])
+   X^Y             % |--> ^(V[X],V[Y])
+   X*Y             % |--> *(V[X],V[Y])
+   X/Y             % |--> /(V[X],V[Y])
+   X\Y             % |--> \(V[X],V[Y])
+   X.^Y            % |--> .^(V[X],V[Y])
+   X.*Y            % |--> .*(V[X],V[Y])
+   X./Y            % |--> ./(V[X],V[Y])
+   X.\Y            % |--> .\(V[X],V[Y])
+   X:Y:Z           % |--> colon(V[X],V[Y],V[Z])
+   X:Z             % |--> colon(V[X],V[Z])
+   X>Z             % |--> >(V[X],V[Y])
+   X>=Z            % |--> >=(V[X],V[Y])
+   X<Z             % |--> <(V[X],V[Y])
+   X=<Z            % |--> <=(V[X],V[Y])
+   X==Z            % |--> ==(V[X],V[Y])
+   [X1,X2,...]     % |--> [ V[X1], V[X2], ... ]
 
-	'Inf'           % |--> Inf (positive infinity)
-	'NaN'           % |--> NaN (not a number)
-	X`              % |--> ctranpose(V[X]) (conjugate transpose, V[X]')
-	X`Y             % |--> V[X].V[q(Y)]
-   X\\Y            % |--> (V[X]) -> V[Y]
-	q(X)            % wrap V[X] in single quotes (escaping internal quotes)
-	qq(X)           % wrap V[X] in double quotes (escaping internal double quotes)
-	tq(X)           % wrap TeX expression in single quotes (escape internal quotes)
-	==
+   'Inf'           % |--> Inf (positive infinity)
+   'NaN'           % |--> NaN (not a number)
+   X`              % |--> ctranpose(V[X]) (conjugate transpose, V[X]')
+   X`Y             % |--> V[X].V[q(Y)]
+   X\\Y            % |--> (V[X]) -> V[Y])
+   q(X)            % wrap V[X] in single quotes (escaping internal quotes)
+   qq(X)           % wrap V[X] in double quotes (escaping internal double quotes)
+   tq(X)           % wrap TeX expression in single quotes (escape internal quotes)
+   ==
 
-	==
-	arr(Lists)         % multidimensional array from nested lists.
-	arr(Lists,Dims)    % multidimensional array from nested lists.
-	==
+   ==
+   arr(Lists)         % multidimensional array from nested lists.
+   arr(Lists,Dims)    % multidimensional array from nested lists.
+   ==
 
-	Things to bypass default formatting
-	==
-	noeval(_)          % triggers a failure when processed
-	\(P)               % escape and call phrase P directly to generate Matlab string
-	$(X)               % calls pl2jl_hook/2, denotes V[Y] where pljl_hook(X,Y).
-	'$VAR'(N)          % gets formatted as p_N where N is assumed to be atomic.
-	==
+   Things to bypass default formatting
+   ==
+   noeval(_)          % triggers a failure when processed
+   \(P)               % escape and call phrase P directly to generate Matlab string
+   $(X)               % calls pl2jl_hook/2, denotes V[Y] where pljl_hook(X,Y).
+   '$VAR'(N)          % gets formatted as p_N where N is assumed to be atomic.
+   ==
 
-	All other Prolog atoms are written using write/1, while other Prolog terms
-	are assumed to be calls to functions named according to the head functor.
-	Thus V[ <head>( <arg1>, <arg2>, ...) ] = <head>(V[<arg1>, V[<arg2>], ...).
+   All other Prolog atoms are written using write/1, while other Prolog terms
+   are assumed to be calls to functions named according to the head functor.
+   Thus V[ <head>( <arg1>, <arg2>, ...) ] = <head>(V[<arg1>, V[<arg2>], ...).
 
-	@tbd
+   @tbd
 
-	Expression language: arr(Vals,Shape,InnerFunctor) - allows efficient
-	representation of arrays of arbitrary things. Will require more strict
-	nested list form.
+   Expression language: arr(Vals,Shape,InnerFunctor) - allows efficient
+   representation of arrays of arbitrary things. Will require more strict
+   nested list form.
 */
 
 :- use_module(library(dcg_core)).
@@ -278,8 +274,8 @@ atm(A,C,T) :- format(codes(C,T),'~w',[A]).
 varnames(L) :- varnames(1,L).
 varnames(_,[]).
 varnames(N,[TN|Rest]) :-
-	atom_concat(p_,N,TN), succ(N,M),
-	varnames(M,Rest).
+   atom_concat(p_,N,TN), succ(N,M),
+   varnames(M,Rest).
 
 
 %% term_jlstring(+X:expr,-Y:list(code)) is det.
@@ -315,5 +311,5 @@ pl2tex(A\\B) --> !, "\\lambda ", pl2tex(A), ".", pl2tex(B).
 pl2tex(abs(A)) --> !, "|", pl2tex(A), "|".
 pl2tex(A)    --> {atomic(A)}, escape_with(0'\\,0'_,at(A)).
 pl2tex(A) -->
-	{compound(A), A=..[H|T] },
-	pl2tex(H), paren(seqmap_with_sep(", ",pl2tex,T)).
+   {compound(A), A=..[H|T] },
+   pl2tex(H), paren(seqmap_with_sep(", ",pl2tex,T)).
